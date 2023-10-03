dd51d38b9d793e7a521c0cfe6e749b92e0b8ecef
jpa5886 <jpa5886@psu.edu>
2023-10-03 19:18:18 -0400

    Initial updates for project deliverable

---
+++ src/main/java/edu/psgv/sweng861/Music.java
@@ -0,0 +1,14 @@
+package edu.psgv.sweng861;
+
+public class Music 
+{
+
+	private static String clientId = "4f6fa41ff16c4f15bce09ae2ea395895";
+	private static String clientSecret = "b09b7f6c3446441fbdef8927e50515db";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world :)");
+	}
+
+}
